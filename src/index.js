1da1d39e4a92f12bf27bf55704e8f8558b522b70
temachkin11 <temachkin11@gmail.com>
2018-09-13 22:38:16 +0300

    it's done

--- src/index.js
+++ src/index.js
@@ -4,4 +4,27 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+  var j = 0, k = 0, m = 0;
+  var triangles = [];
+  for(var i = 0; i < preferences.length; i++)
+        {
+          if (triangles.includes(preferences[i])) continue;
+          
+          k = preferences[i];
+          j = preferences[k - 1];
+          m = preferences[j - 1];
+          if(k === j) continue;
+          if (k == preferences[m - 1])
+          {
+
+          triangles.push(k);
+          triangles.push(j);
+          triangles.push(m);
+          }
+         
+    
+  }
+  return triangles.length / 3;
+}
+
+  
